47037299517aa880948cac703e8ac436d215e20b
HarshPatiL11 <patilharsh497@gmail.com>
2024-10-01 15:48:35 +0530

    added Vite

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify"; // Import ToastContainer
+import "react-toastify/dist/ReactToastify.css"; // Import Toastify CSS
 
-createRoot(document.getElementById('root')).render(
+import LoginPage from "./Components/Pages/User/Login.jsx";
+import Register from "./Components/Pages/User/Register.jsx";
+import UserHome from "./Components/Pages/User/UserHome.jsx";
+
+// Define routes using createBrowserRouter
+const router = createBrowserRouter([
+  { path: "/", element: <UserHome/> },
+  { path: "/user/login", element: <LoginPage /> },
+  { path: "/user/register", element: <Register /> },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* Provide the router configuration */}
+    <RouterProvider router={router} />
+    <ToastContainer />
+  </StrictMode>
+);
